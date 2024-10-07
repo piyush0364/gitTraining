61edb4045a825675e718463d75fc61014f8c5ae3
unknown <tripathipiyush456@gmail.com>
2024-10-07 16:37:36 +0530

    Second Commit

---
+++ WebAppMVCGit/Models/Emp.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAppMVCGit.Models
+{
+    public class Emp
+    {
+        public int EId { get; set; }
+        public string EName { get; set; }
+        public string EDesign {  get; set; }
+        public DateTime EDOJ { get; set; }
+    }
+}
